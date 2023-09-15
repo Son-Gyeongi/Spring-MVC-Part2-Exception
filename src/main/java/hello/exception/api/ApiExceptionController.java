3e67f0dfe717bdcd2b22c34e3310897c04c65b79
Son-Gyeongi <thsruddl77@gmail.com>
2023-09-15 16:25:34 +0900

    API 예외 처리 - 스프링이 제공하는 ExceptionResolver1 (ResponseStatusExceptionResolver, ex2)

--- src/main/java/hello/exception/api/ApiExceptionController.java
+++ src/main/java/hello/exception/api/ApiExceptionController.java
@@ -5,9 +5,11 @@ import hello.exception.exception.UserException;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.server.ResponseStatusException;
 
 // api 호출 예제
 @Slf4j
@@ -34,10 +36,17 @@ public class ApiExceptionController {
         return new MemberDto(id, "hello " + id);
     }
 
-    // API 예외 처리 - 스프링이 제공하는 ExceptionResolver1 (ResponseStatusExceptionResolver)
+    // API 예외 처리 - 스프링이 제공하는 ExceptionResolver1 (ResponseStatusExceptionResolver, ex1)
     @GetMapping("/api/response-status-ex1")
     public String responseStatusEx1() {
-        throw new BadRequestException();
+        throw new BadRequestException(); // 에러 400
+    }
+
+    // API 예외 처리 - 스프링이 제공하는 ExceptionResolver1 (ResponseStatusExceptionResolver, ex2)
+    @GetMapping("/api/response-status-ex2")
+    public String responseStatusEx2() {
+        // 상태코드랑 오류 메시지까지 한번에 해결할 수 있는 ResponseStatusException()
+        throw new ResponseStatusException(HttpStatus.NOT_FOUND, "error.bad", new IllegalArgumentException()); // 에러 404
     }
 
     @Data
